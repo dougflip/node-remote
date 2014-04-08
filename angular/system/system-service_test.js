e4091b3b5739cdf1c08d9e1ba65e7eb351c9e309
dougflip <douglasdifilippo@gmail.com>
2014-04-08 09:31:15 -0400

    Cleans up the service tests

--- angular/system/system-service_test.js
+++ angular/system/system-service_test.js
@@ -17,4 +17,25 @@ describe('SystemService', function(){
       $httpBackend.flush();
     });
   });
+
+  describe('when setting the volume', function(){
+    it('should set the new volume level', function(){
+      sut.setVolume(50);
+      expect(sut.volume).toEqual(50);
+    });
+
+    it('should post the volume to the correct API endpoint', function(){
+      $httpBackend.expectPOST('/system/setVolume', { level: 50 }).respond(200);
+      sut.setVolume(50);
+      $httpBackend.flush();
+    });
+  });
+
+  describe('when calling suspend', function(){
+    it('should post to the correct API endpoint', function(){
+      $httpBackend.expectPOST('/system/suspend').respond(200);
+      sut.suspend();
+      $httpBackend.flush();
+    });
+  });
 });
