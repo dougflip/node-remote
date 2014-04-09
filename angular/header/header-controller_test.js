e3ef0905412eee2efdc4d3dcad972004ff6b5583
dougflip <douglasdifilippo@gmail.com>
2014-04-09 09:15:07 -0400

    Adds a test for the header controller

---
+++ angular/header/header-controller_test.js
@@ -0,0 +1,23 @@
+describe('HeaderController', function(){
+  var mocks = require('../angular-mock');
+  var sut, rootScopeMock;
+
+  beforeEach(mocks.module(require('../node-remote').name));
+
+  beforeEach(mocks.module(function($provide){
+    rootScopeMock = { $emit: new Function() };
+    $provide.value('$rootScope', rootScopeMock);
+  }));
+
+  beforeEach(mocks.inject(function($controller){
+    sut = $controller('headerCtrl');
+  }));
+
+  describe('when opening the main menu', function(){
+    it('should call $emit on the root scope', function(){
+      spyOn(rootScopeMock, '$emit');
+      sut.openMainMenu();
+      expect(rootScopeMock.$emit).toHaveBeenCalledWith('menu:open');
+    });
+  });
+});
