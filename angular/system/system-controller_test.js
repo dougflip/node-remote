87297f53d63e2a897ed88f3f06c0c2902a285e1e
dougflip <douglasdifilippo@gmail.com>
2014-04-14 09:03:01 -0400

    Fixes the system controller tests

--- angular/system/system-controller_test.js
+++ angular/system/system-controller_test.js
@@ -1,12 +1,13 @@
 describe('SystemController', function(){
   var mocks = require('angular-mock');
-  var sut, systemServiceMock;
+  var sut, systemServiceMock, $scope;
 
   beforeEach(mocks.module(require('../node-remote').name));
 
   beforeEach(mocks.module(function($provide){
     systemServiceMock = {
       volume: 10,
+      onVolumeChange: function(){},
       mute: function(){},
       setVolume: function(){},
       suspend: function(){}
@@ -14,21 +15,34 @@ describe('SystemController', function(){
     $provide.value('systemService', systemServiceMock);
   }));
 
-  beforeEach(mocks.inject(function($controller){
-    sut = $controller('systemCtrl');
+  beforeEach(mocks.inject(function($controller, _$rootScope_){
+    $scope = _$rootScope_.$new();
+    sut = $controller('systemCtrl', { $scope: $scope });
   }));
 
   describe('post construction', function(){
     it('should intialize volume to the system service volume', function(){
       expect(sut.volume).toEqual(10);
     });
+
+    it('should register for volume change', function(){
+      // TODO: like to make sure this event registration happens...
+      //  This is also a great place for an e2e test instead
+      //  Test could switch to this view, change the volume and ensure UI updates
+    });
+  });
+
+  describe('when volume change is emitted', function(){
+    it('should set the local volume', function(){
+      sut.onVolumeChange({ volume: 100 });
+      expect(sut.volume).toEqual(100);
+    });
   });
 
   describe('when setting volume', function(){
     it('should call through to the system service', function(){
       spyOn(systemServiceMock, 'setVolume');
-      sut.volume = 50;
-      sut.setVolume();
+      sut.setVolume(50);
       expect(systemServiceMock.setVolume).toHaveBeenCalledWith(50);
     });
   });
