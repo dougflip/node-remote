c3503c6042337aec016af728153498d9c60ed6c3
dougflip <douglasdifilippo@gmail.com>
2014-04-15 15:17:12 -0400

    Adds more protractor tests
    I am not concerned with structure or anything right now.
    Still just exploring protractor API and getting tests to pass.
    Once I get a few more I will start to break them out across files.

--- angular/e2e/system_e2e.js
+++ angular/e2e/system_e2e.js
@@ -1,23 +1,50 @@
-var systemPage = {
-  get: function(){
-    browser.get('http://localhost:9000');
-  },
-  volumeInput: element(by.model('systemCtrl.volume')),
-  volumeUpBtn: element(by.css('.system--volume-up')),
-  volumeDownBtn: element(by.css('.system--volume-down'))
-};
+describe('Running NodeRemote', function(){
+  var testHelper = require('./test-helper');
 
-describe('System page', function(){
   describe('setting volume up and down', function(){
+    var systemPage;
+    beforeEach(function(){
+      systemPage = new testHelper.SystemModule();
+    });
+
     it('should reflect changes in the UI', function(){
-      systemPage.get();
-      expect(systemPage.volumeInput.getAttribute('value')).toEqual('50');
+      testHelper.getIndex();
+      expect(systemPage.volumeInput().getAttribute('value')).toEqual('50');
 
-      systemPage.volumeUpBtn.click();
-      expect(systemPage.volumeInput.getAttribute('value')).toEqual('60');
+      systemPage.volumeUpBtn().click();
+      expect(systemPage.volumeInput().getAttribute('value')).toEqual('60');
 
-      systemPage.volumeDownBtn.click();
-      expect(systemPage.volumeInput.getAttribute('value')).toEqual('50');
+      systemPage.volumeDownBtn().click();
+      expect(systemPage.volumeInput().getAttribute('value')).toEqual('50');
+    });
+  });
+
+  describe('navigating to other pages', function(){
+    var headerModule, menuModule, browserModule, netflixModule, trackpadModule;
+    beforeEach(function(){
+      headerModule = new testHelper.HeaderModule();
+      menuModule = new testHelper.MenuModule();
+      browserModule = new testHelper.BrowserModule();
+      netflixModule = new testHelper.NetflixModule();
+      trackpadModule = new testHelper.TrackpadModule();
+    });
+
+    it('should navigate to the browser module', function(){
+      headerModule.mainMenuButton().click();
+      menuModule.browserLink().click();
+      expect(browserModule.launchInput().isPresent()).toBe(true);
+    });
+
+    it('should navigate to the netflix module', function(){
+      headerModule.mainMenuButton().click();
+      menuModule.netflixLink().click();
+      expect(netflixModule.searchInput().isPresent()).toBe(true);
+    });
+
+    it('should navigate to the trackpad module', function(){
+      headerModule.mainMenuButton().click();
+      menuModule.trackpadLink().click();
+      expect(trackpadModule.sendTextInput().isPresent()).toBe(true);
     });
   });
 });
