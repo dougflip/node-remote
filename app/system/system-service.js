6f13c88ff7c42e046a607f12f0c44c0ab167e6a0
dougflip <douglasdifilippo@gmail.com>
2015-09-16 14:22:26 -0400

    Upgrades service to es2015 class

--- app/system/system-service.js
+++ app/system/system-service.js
@@ -1,35 +1,38 @@
 var ng = require('angular');
 
-function SystemService($http, Events){
-  this.http = $http;
+class SystemService {
 
-  // the UI component binds to this directly
-  // which is a little dirty, but keeping it simple for now...
-  this.volume = 50;
+  constructor($http){
+    this.http = $http;
+
+    // the UI component binds to this directly
+    // which is a little dirty, but keeping it simple for now...
+    this.volume = 50;
+  }
+
+  mute(){
+    this.volume = 0;
+    return this.http.post('/system/mute');
+  }
+
+  setVolume(level){
+    level = parseNewLevel(level);
+
+    if(level === null) return;
+
+    this.volume = level;
+    return this.http.post('/system/set-volume', { level: this.volume });
+  }
+
+  closeWindow(){
+    return this.http.post('/system/close-window');
+  }
+
+  suspend(){
+    return this.http.post('/system/suspend');
+  }
+
 }
-
-SystemService.prototype.mute = function(){
-  this.volume = 0;
-  this.emitVolumeChange();
-  return this.http.post('/system/mute');
-};
-
-SystemService.prototype.setVolume = function(level){
-  level = parseNewLevel(level);
-
-  if(level === null) return this.emitVolumeChange();
-
-  this.volume = level;
-  return this.http.post('/system/set-volume', { level: this.volume });
-};
-
-SystemService.prototype.closeWindow = function(){
-  return this.http.post('/system/close-window');
-};
-
-SystemService.prototype.suspend = function(){
-  return this.http.post('/system/suspend');
-};
 
 function parseNewLevel(level){
   level = parseInt(level);
@@ -42,4 +45,4 @@ function parseNewLevel(level){
   return level;
 }
 
-module.exports = ['$http', 'Events', SystemService];
+module.exports = ['$http', SystemService];
