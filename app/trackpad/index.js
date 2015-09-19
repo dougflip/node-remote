25ef12f24df1bc56035f7be93d1c2853d5025e09
dougflip <douglasdifilippo@gmail.com>
2015-09-18 21:30:37 -0400

    WIP: Working on a touchpad directive
    This is the base building block for a slightly larger directive.
    One that will include the touchpad and the mouse buttons.

--- app/trackpad/index.js
+++ app/trackpad/index.js
@@ -2,5 +2,6 @@ var ng = require('angular');
 
 module.exports = ng.module('trackpad', [])
   .controller('trackpadCtrl', require('./trackpad-controller'))
+  .directive('touchpad', require('./touchpad'))
   .factory('DragCalculator', require('./drag-calculator'))
   .service('trackpadService', require('./trackpad-service'));
