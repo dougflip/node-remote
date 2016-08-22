f3c78c0239f3685cb666e923aaf41b2406e2e157
Doug DiFilippo <ddifilippo@wespeke.com>
2016-08-22 19:20:30 -0400

    Starts to get the mousepad rolling

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
+import hammerjs from 'hammerjs';
+import hmTouchEvents from 'angular-hammer';
 import nodeRemoteMods from './modules/modules';
 
-
 const nodeRemoteModule = angular.module('nodeRemote', [
+  'hmTouchEvents',
   nodeRemoteMods.name
 ]);
 
--- src/modules/mousepad/mousepad-component.js
+++ src/modules/mousepad/mousepad-component.js
@@ -1,8 +1,24 @@
+/**
+* Outputs drag movement in the form of { deltaX, deltaY }
+* This is designed to be used to send as relative mouse movement.
+*/
 class Mousepad {
   constructor() {}
+
+  onDrag(evt) {
+    const deltaX = evt.velocityX * 10;
+    const deltaY = evt.velocityY * 10;
+    this.onMove({ evt, deltaX, deltaY });
+  }
 }
 
 export default {
   template: require('./mousepad-component.html'),
-  controller: Mousepad
+  controller: Mousepad,
+  bindings: {
+    onMove: '&',
+    onLeftClick: '&',
+    onDoubleClick: '&',
+    onRightClick: '&'
+  }
 };
